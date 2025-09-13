543fe446aa098efa9e6d5b4ade2272c5b1a5eb91
Buzurgmehr1 <bsftraders4@gmail.com>
2025-09-13 12:58:49 +0500

    models created

---
+++ models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str]
+    role = Mapped[str] = mapped_column(default='user')
+
+class Author(Base):
+    __tablename__ = "authors"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique= True)
+    bio: Mapped[str]
+    books: Mapped[list["Book"]] = relationship("Book", back_populates="author")
+
+class Book(Base):
+    __tablename__ = "books"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    year: Mapped[int]
+    available: Mapped[bool] = mapped_column(default=True)
+    author_id: Mapped[int] = mapped_column(ForeignKey("authors.id"))
+    author: Mapped[Author] = relationship("Author", back_populates="books")
